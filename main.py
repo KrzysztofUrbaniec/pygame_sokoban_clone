4896c989ccf6ea9485a99d6a575ebfa90ec202d8
Krzysztof <urbaniec.krzysztof@interia.pl>
2022-02-20 22:47:07 +0100

    Added player movement, pushing boxes and collision detection between player, boxes and walls

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 import pygame, sys
 from pygame.locals import *
-from map import LevelMap
+from map import LevelMap, Floor, Box, Wall
 from constants import *
 from entities import Player
 import logging
+from debug import debug
+
+logging.basicConfig(level=logging.DEBUG, format='%(levelname)s:%(message)s')
+
+LEFT = 'left'
+RIGHT = 'right'
+UP = 'up'
+DOWN = 'down'
 
 def main():
     global DISPLAYSURF, SCREENSURF, FPSCLOCK
@@ -22,19 +30,84 @@ def main():
 
     while True:
         SCREENSURF.fill(LIGHTBLUE)
-        
+
+        direction = None
+
         for event in pygame.event.get():
             if event.type == QUIT:
                 pygame.quit()
                 sys.exit()
+            
+            # Player movement and changing position
+            if event.type == KEYDOWN:
+                if event.key == K_LEFT:
+                    player_rect.x -= level1.tile_size
+                    direction = LEFT
+                if event.key == K_RIGHT:
+                    player_rect.x += level1.tile_size
+                    direction = RIGHT
+                if event.key == K_UP:
+                    player_rect.y -= level1.tile_size
+                    direction = UP
+                if event.key == K_DOWN:
+                    player_rect.y += level1.tile_size
+                    direction = DOWN
 
-        level1.draw_map()
+        # Boxes movement
+        for box in level1.boxes:
+            if box.rect.x == player_rect.x and box.rect.y == player_rect.y:
+                if direction == DOWN:
+                    box.rect.y += level1.tile_size
+                if direction == UP:
+                    box.rect.y -= level1.tile_size
+                if direction == LEFT:
+                    box.rect.x -= level1.tile_size
+                if direction == RIGHT:
+                    box.rect.x += level1.tile_size
+
+            # Check if moved box collides with walls - if yes, then return in to previous position
+            for tile in level1.tiles:
+                if box.rect.x == tile.rect.x and box.rect.y == tile.rect.y and isinstance(tile, Wall):
+                    if direction == DOWN:
+                        box.rect.y -= level1.tile_size
+                        player_rect.y -= level1.tile_size
+                    if direction == UP:
+                        box.rect.y += level1.tile_size
+                        player_rect.y += level1.tile_size
+                    if direction == LEFT:
+                        box.rect.x += level1.tile_size
+                        player_rect.x += level1.tile_size
+                    if direction == RIGHT:
+                        box.rect.x -= level1.tile_size
+                        player_rect.x -= level1.tile_size
+
+        # Collisions with other tiles
+        for rect in level1.tiles:
+            if rect.x == player_rect.x and rect.y == player_rect.y and isinstance(rect, Wall):
+                if direction == DOWN:
+                    player_rect.y -= level1.tile_size
+                if direction == UP:
+                    player_rect.y += level1.tile_size
+                if direction == LEFT:
+                    player_rect.x += level1.tile_size
+                if direction == RIGHT:
+                    player_rect.x -= level1.tile_size
+
+        # TODO: If box collides with boxspot - set box attribute 'in_right_spot' to True; if all boxes are in proper spots - player wins
+        
+        # TODO: Add win conditions
+
+        # TODO: Add level reset function
+
+        level1.draw_map() 
+
         SCREENSURF.blit(player_img, player_rect)
 
         scaled_surf = pygame.transform.scale(SCREENSURF, DISPLAYSURF.get_size())
         DISPLAYSURF.blit(scaled_surf, (0,0))
         pygame.display.update()
         FPSCLOCK.tick(FPS)
+
 
 if __name__ == '__main__':
     main()
